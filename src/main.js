cefd67d2c5e7249f89749a81349531c4feb3a64d
Andrrr <andresin@gmail.com>
2018-04-02 13:24:34 -0300

    Main app root

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
+import Vuex from "vuex";
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import VueSocketio from 'vue-socket.io';
+Vue.use(VueSocketio, 'http://localhost:3000');
+
+// Configs
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 
+Vue.use(Vuex);
+
+import store from './store.js';
+
+// Components
+let IndexComponent = Vue.extend({
+  template: '#index',
+  props: ['posts'],
+});
+
+let VideosComponent = Vue.extend({
+  template: '#videos',
+  props: ["counter"]
+});
+
+Vue.component("index", IndexComponent);
+Vue.component("videos", VideosComponent);
+
+// Ah Vue!
 new Vue({
+  store, // ES6 equivalent of store: store
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+// vm.$store(store);
